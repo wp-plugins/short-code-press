a2e96dcea9ca45a4c39d2b4bbf9aa40f44fccd23
shaid <shaid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-23 11:40:48 +0000

    git-svn-id: https://plugins.svn.wordpress.org/short-code-press/trunk@509312 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcodepress.php
+++ shortcodepress.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: ShortCode Press
-Version: 1.0.1
+Version: 1.0.2
 Plugin URI: http://www.shortcodepress.com
 Author URI: http://www.shortcodepress.com
 Description:  insert custom shortcodes to your post or pages fast and easy way.
