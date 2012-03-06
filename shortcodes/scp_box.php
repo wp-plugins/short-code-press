e94e86ea6fb91c8b1bdd3f42c022d71e586dc531
shaid <shaid@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-06 11:21:59 +0000

    git-svn-id: https://plugins.svn.wordpress.org/short-code-press/trunk@515238 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- shortcodes/scp_box.php
+++ shortcodes/scp_box.php
@@ -11,7 +11,7 @@ function scp_box_shortcode( $atts, $content = null )
       'align' => '',
       ), $atts ) );
 
-      if($template!='')$tpl=' drop-shadow '.$template.' '.$color.' ';
+      if($template!='')$tpl=' drop-shadow '.$template.' ';
       return '<style type="text/css">.scp_box_'.$width.'{width:'.$width.'px;}</style><div class="box_container scp_'. $align.'" style="width:'.($width+20).'px"><div class="scp scp_box scp_box_' . $width . ' scp_box_' . $color . ' '. $tpl.'">' . $content . '</div></div>';
 
 }
